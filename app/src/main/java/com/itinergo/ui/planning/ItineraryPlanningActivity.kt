c71da457eeb8059f048e6f456127f148be9f144b
zainalbani <zainalbani007@gmail.com>
2023-11-27 19:57:27 +0700

    update: layout itinerary planning

---
+++ app/src/main/java/com/itinergo/ui/planning/ItineraryPlanningActivity.kt
@@ -0,0 +1,12 @@
+package com.itinergo.ui.planning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.itinergo.R
+
+class ItineraryPlanningActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_itinerary_planning)
+    }
+}
