1b73e406da478b885c3c6fceb1fd2d7af27cdc87
zainalbani <zainalbani007@gmail.com>
2023-12-04 10:09:19 +0700

    fix: layout revise

---
+++ app/src/main/java/com/itinergo/ui/verification/VerificationActivity.kt
@@ -0,0 +1,12 @@
+package com.itinergo.ui.verification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.itinergo.R
+
+class VerificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verification)
+    }
+}
